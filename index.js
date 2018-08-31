4765cbaf418612e77e99219905aba13bf5898698
MRhanrui <632836845@qq.com>
2018-08-31 17:41:56 +0800

    1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const sever = express();
+sever.use(express,static('./www'));
+sever.get('/',(req,res)=>{
+    res.sendFile(
+        path.resolve('./view/index.html')
+    )
+    }
+)
+sever.listen(80)
